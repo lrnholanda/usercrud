5f260966ed7d3f7c5bb8999838c05e71714a3e82
lrnholanda <lrnholanda@gmail.com>
2022-06-15 12:48:46 -0300

    added prima module & service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,14 @@
 /* eslint-disable prettier/prettier */
 // eslint-disable-next-line prettier/prettier
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService){
+    
+  }
 signup() {
   return { msg: 'I have signed up'};
 }
